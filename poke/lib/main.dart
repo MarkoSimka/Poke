ee4f12cf934cac3a1fe4aa50d229ddbd52b8ac5b
Mario Oklevski <oklevskimarioo@gmail.com>
2023-09-17 20:07:03 +0200

    [MO] Main setup

--- poke/lib/main.dart
+++ poke/lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'firebase_options.dart';
+import 'package:poke/screens/home.dart';
 
 Future<void> main() async {
-  runApp(const MainApp());
+  runApp(const MyApp());
   
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      builder: (context, child) => MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child!),
+      debugShowCheckedModeBanner: false,
+      title: 'Poke',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      initialRoute: 'Home',
+      routes: {
+        'Home': (context) => HomeScreen(),
+        // 'Login': (context) => const LoginScreen(), // Login
+        // 'Register': (context) => const RegisterScreen(), // Login
+      },
     );
   }
 }
